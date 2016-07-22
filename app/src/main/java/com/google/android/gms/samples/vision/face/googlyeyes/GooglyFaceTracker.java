2e8f1df878828a6b28475e80611c7d8a53e274cc
ChinTseng <Chin@ChinTsengdeMacBook-Pro.local>
2016-07-23 00:35:39 +0800

    first commit

---
+++ app/src/main/java/com/google/android/gms/samples/vision/face/googlyeyes/GooglyFaceTracker.java
@@ -0,0 +1,246 @@
+/*
+ * Copyright (C) The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.google.android.gms.samples.vision.face.googlyeyes;
+
+import android.app.admin.DeviceAdminInfo;
+import android.content.Context;
+import android.content.ContextWrapper;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.ImageFormat;
+import android.graphics.PointF;
+import android.graphics.Rect;
+import android.graphics.YuvImage;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.Environment;
+import android.util.Log;
+import android.view.View;
+
+import com.google.android.gms.samples.vision.face.googlyeyes.ui.camera.GraphicOverlay;
+import com.google.android.gms.vision.Tracker;
+import com.google.android.gms.vision.face.Face;
+import com.google.android.gms.vision.face.FaceDetector;
+import com.google.android.gms.vision.face.Landmark;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.ByteBuffer;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Tracks the eye positions and state over time, managing an underlying graphic which renders googly
+ * eyes over the source video.<p>
+ *
+ * To improve eye tracking performance, it also helps to keep track of the previous landmark
+ * proportions relative to the detected face and to interpolate landmark positions for future
+ * updates if the landmarks are missing.  This helps to compensate for intermediate frames where the
+ * face was detected but one or both of the eyes were not detected.  Missing landmarks can happen
+ * during quick movements due to camera image blurring.
+ */
+class GooglyFaceTracker extends Tracker<Face> {
+    private static final float EYE_CLOSED_THRESHOLD = 0.4f;
+
+    private GraphicOverlay mOverlay;
+    private GooglyEyesGraphic mEyesGraphic;
+
+    // Record the previously seen proportions of the landmark locations relative to the bounding box
+    // of the face.  These proportions can be used to approximate where the landmarks are within the
+    // face bounding box if the eye landmark is missing in a future update.
+    private Map<Integer, PointF> mPreviousProportions = new HashMap<>();
+
+    // Similarly, keep track of the previous eye open state so that it can be reused for
+    // intermediate frames which lack eye landmarks and corresponding eye state.
+    private boolean mPreviousIsLeftOpen = true;
+    private boolean mPreviousIsRightOpen = true;
+
+
+    //==============================================================================================
+    // Methods
+    //==============================================================================================
+
+    GooglyFaceTracker(GraphicOverlay overlay) {
+        mOverlay = overlay;
+    }
+
+    /**
+     * Resets the underlying googly eyes graphic and associated physics state.
+     */
+    @Override
+    public void onNewItem(int id, Face face) {
+
+
+
+        mEyesGraphic = new GooglyEyesGraphic(mOverlay);
+    }
+
+    /**
+     * Updates the positions and state of eyes to the underlying graphic, according to the most
+     * recent face detection results.  The graphic will render the eyes and simulate the motion of
+     * the iris based upon these changes over time.
+     */
+    private boolean flag = true;
+    @Override
+    public void onUpdate(FaceDetector.Detections<Face> detectionResults, Face face) {
+        mOverlay.add(mEyesGraphic);
+
+        updatePreviousProportions(face);
+
+        PointF leftPosition = getLandmarkPosition(face, Landmark.LEFT_EYE);
+        PointF rightPosition = getLandmarkPosition(face, Landmark.RIGHT_EYE);
+
+        float leftOpenScore = face.getIsLeftEyeOpenProbability();
+        boolean isLeftOpen;
+        if (leftOpenScore == Face.UNCOMPUTED_PROBABILITY) {
+            isLeftOpen = mPreviousIsLeftOpen;
+        } else {
+            isLeftOpen = (leftOpenScore > EYE_CLOSED_THRESHOLD);
+            mPreviousIsLeftOpen = isLeftOpen;
+        }
+
+        float rightOpenScore = face.getIsRightEyeOpenProbability();
+        boolean isRightOpen;
+        if (rightOpenScore == Face.UNCOMPUTED_PROBABILITY) {
+            isRightOpen = mPreviousIsRightOpen;
+        } else {
+            isRightOpen = (rightOpenScore > EYE_CLOSED_THRESHOLD);
+            mPreviousIsRightOpen = isRightOpen;
+        }
+
+        if(flag) {
+            View v = mOverlay;
+            int a = mOverlay.getLayoutParams().width;
+            String s = Integer.toString(a);
+            Log.d("dddddddddd", s);
+            /*Bitmap b = Bitmap.createBitmap( v.getLayoutParams().width, v.getLayoutParams().height, Bitmap.Config.ARGB_8888);
+            Canvas c = new Canvas(b);
+            v.layout(0, 0, v.getLayoutParams().width, v.getLayoutParams().height);
+            v.draw(c);
+
+            if(b == null) { Log.d("jean", "null!!!!!!"); }
+            else {
+                int[] pixs = new int[b.getHeight() * b.getWidth()];
+                int w = b.getWidth();
+                int h = b.getHeight();
+                String s1 = Integer.toString(w);
+                String s2 = Integer.toString(h);
+                Log.d("jean wwwwwwwwww", s1);
+                Log.d("jean hhhhhhhhhh", s2);
+
+                b.getPixels(pixs, 0, w, 0, 0, w, h);
+
+
+                for (int i = 0; i < 60; i++) {
+                    for (int j = 2; j < 5; j++) {
+                        int tmp = b.getPixel(i, j);
+                        String s = Integer.toString(tmp);
+                        String num = Integer.toString(i);
+                        Log.d(num, s);
+                    }
+                }
+            }*/
+
+            flag = false;
+        }
+
+        mEyesGraphic.updateEyes(leftPosition, isLeftOpen, rightPosition, isRightOpen);
+
+
+    }
+
+
+
+    /**
+     * Hide the graphic when the corresponding face was not detected.  This can happen for
+     * intermediate frames temporarily (e.g., if the face was momentarily blocked from
+     * view).
+     */
+    @Override
+    public void onMissing(FaceDetector.Detections<Face> detectionResults) {
+        mOverlay.remove(mEyesGraphic);
+    }
+
+    /**
+     * Called when the face is assumed to be gone for good. Remove the googly eyes graphic from
+     * the overlay.
+     */
+    @Override
+    public void onDone() {
+        mOverlay.remove(mEyesGraphic);
+    }
+
+    //==============================================================================================
+    // Private
+    //==============================================================================================
+
+    private void updatePreviousProportions(Face face) {
+        for (Landmark landmark : face.getLandmarks()) {
+            PointF position = landmark.getPosition();
+            float xProp = (position.x - face.getPosition().x) / face.getWidth();
+            float yProp = (position.y - face.getPosition().y) / face.getHeight();
+            mPreviousProportions.put(landmark.getType(), new PointF(xProp, yProp));
+        }
+    }
+
+    /**
+     * Finds a specific landmark position, or approximates the position based on past observations
+     * if it is not present.
+     */
+    private PointF getLandmarkPosition(Face face, int landmarkId) {
+        for (Landmark landmark : face.getLandmarks()) {
+            if (landmark.getType() == landmarkId) {
+                return landmark.getPosition();
+            }
+        }
+
+        PointF prop = mPreviousProportions.get(landmarkId);
+        if (prop == null) {
+            return null;
+        }
+
+        float x = face.getPosition().x + (prop.x * face.getWidth());
+        float y = face.getPosition().y + (prop.y * face.getHeight());
+        return new PointF(x, y);
+    }
+
+
+
+
+    /*private Bitmap takeScreenShot(View view) {
+        // configuramos para que la view almacene la cache en una imagen
+        view.setDrawingCacheEnabled(true);
+        view.setDrawingCacheQuality(View.DRAWING_CACHE_QUALITY_LOW);
+        view.buildDrawingCache();
+
+        if(view.getDrawingCache() == null) return null; // Verificamos antes de que no sea null
+
+        // utilizamos esa cache, para crear el bitmap que tendra la imagen de la view actual
+        Bitmap snapshot = Bitmap.createBitmap(view.getDrawingCache());
+        view.setDrawingCacheEnabled(false);
+        view.destroyDrawingCache();
+
+        return snapshot;
+    }*/
+}
